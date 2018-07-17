b6aa7a38afffcc0a9ac5bc0cd4035c1b1a033ab8
Christian Jean <christian.jean@gmail.com>
2018-07-17 00:01:24 -0400

    Updated our library with recent code (was missing basic functionality)

--- server/jeach-pqueues.js
+++ server/jeach-pqueues.js
@@ -1,6 +1,4 @@
 /*
- * Jeach Promise Queues for Node JS
- *
  * Copyright (C) 2018 by Christian Jean.
  * All rights reserved.
  *
@@ -23,18 +21,31 @@
  */
 
 
-var QUEUE_STATE = {
+/**
+  TODO Section - Some of the many things that remain to be done!
+  ------------------------------------------------------------------
+  Add a 'leniency' variable which will allow leniant execution (or 
+  not). When set to leniant for example, the use of 'q.ref(...)'
+  will not enforce that the parameter is already set. When not 
+  leniant, a check wold ocurr and throw an exception if not found.
+*/
+
+const QUEUE_STATE = {
   PENDING: 0,
   STARTED: 1,
   FINISHED: 9
 };
 
 var queues = [];
+var logLevel = 0; // 0=none, 1=info, 2=some, 3=full
 
 //----------------------------------------------------------------------------------------
 
 function PQueue() {
   this.deferred = [];
+  this.caught = [];
+  this.finals = [];
+
   this.stack = new PStack();
   this.state = QUEUE_STATE.PENDING;
 }
@@ -42,7 +53,7 @@ function PQueue() {
 PQueue.prototype.add = function() {
   var refs = 0;
   var len = arguments.length;
-  
+
   if (len < 2) throw new Error("Need at least 2 arguments: PQueue.add(fn, [arguments, ...], callback);");
   if (typeof arguments[0] !== "function") throw new Error("First argument is not of type Function (deferred)!");
   if (typeof arguments[len-1] !== "function") throw new Error("Last argument is not of type Function (callback)!");
@@ -57,44 +68,146 @@ PQueue.prototype.add = function() {
   var rt = (refs == 0 ? "" : ", " + refs + " ref" + (refs > 1 ? "s" : ""));
   var at = (len <= 2 ? "" : ", [" + (len - 2) + " arg" + (len-2 > 1 ? "s" : "") + rt + "]" );
   
-  console.log("PQueue.add(fn" + at + ", cb);");
-  
   this.deferred.push({ fn: arguments[0], args: args, cb: arguments[len-1] });
+
+  log(1, "PQueue.add(fn" + at + ", cb), deferred: " + this.deferred.length);
+}
+
+PQueue.prototype.catch = function() {
+  if (typeof arguments[0] !== "function") throw new Error("First argument is not of type Function (catch)!");
+  if (this.caught.length > 0) throw new Error("Only one PQueue.catch(fn) allowed (at this time)!");
+  this.caught.push({ cb: arguments[0] });
+}
+
+PQueue.prototype.finally = function() {
+  if (typeof arguments[0] !== "function") throw new Error("First argument is not of type Function (finally)!");
+  this.finals.push({ cb: arguments[0] });
 }
 
 PQueue.prototype.toString = function() {
-  return "Queue: state: " + this.state + ", stack: " + this.stack.getSize() + ", deferred: " + this.deferred.length;
+  return "Queue: state: " + this.state +
+           ", stack: " + this.stack.getSize() +
+           ", deferred: " + this.deferred.length + 
+           ", caught: " + this.caught.length + 
+           ", finals: " + this.finals.length + 
+           ", debug: " + (logLevel > 0 ? true : false) +
+           ", level: " + logLevel;
 }
 
 PQueue.prototype.exec = function() {
-  console.log("------------------------------------------------------------------");
-  console.log("Starting execution:");
-  console.log(" > " + this.toString());
+  log(1, "Starting execution:");
+  log(2, " > " + this.toString());
   
   this.state = QUEUE_STATE.STARTED;
-  
-  for (var i=0; i<this.deferred.length; i++) {
-    console.log(" > Invoking deferred " + (i + 1) + " of " + this.deferred.length);
-    var fn = this.deferred[i].fn;
-    var cb = this.deferred[i].cb;
-    var args = [];
+
+  //this.execDeferred(0);
+  setTimeout(this.execDeferred, 1, this, 0);
+}
+
+PQueue.prototype.execDeferred = function(queue, i) {
+  if (i < 0) throw new Error("Invalid 'deferred' reference index!");
+
+  if (i >= queue.deferred.length) {
+    log(1, " > Completed all deferred, now invoking finals!");
+    setTimeout(queue.execFinals, 1, queue, 0);
+    return;
+  }
+
+  log(3, " > ---------------------------------------------------------------");
+  log(2, " > Invoking deferred #" + (i+1));
+
+  var fn = queue.deferred[i].fn;
+  var cb = queue.deferred[i].cb;
+  var args = [];
     
-    args.push(this.stack); // first arg is our stack!
+  for (var a=0; a<queue.deferred[i].args.length; a++) {
+    if (queue.deferred[i].args[a] instanceof PStackRef) {
+      args.push(queue.deferred[i].args[a].resolve());
+    } else {
+      args.push(queue.deferred[i].args[a]);
+    }
+  }
     
-    for (var a=0; a<this.deferred[i].args.length; a++) {
-      if (this.deferred[i].args[a] instanceof PStackRef) {
-        args.push(this.deferred[i].args[a].resolve());
-      } else {
-        args.push(this.deferred[i].args[a]);
+  var fcb = function(error, data) {
+    log(2, "Invoked deferred:");
+    log(2, " > Error: " + JSON.stringify(error));
+    log(2, " > Param: " + JSON.stringify(data));
+
+    if (error) {
+      log(1, " > Received error, deferring to caught chain!");
+      setTimeout(queue.execCaught, 1, queue, error, 0);
+    } else {
+      try {
+        var args = [ queue.stack, data ];
+        log(3, " > Now invoking callback w/ " + args.length + " arg" + (args.length > 1 ? "s" : "")  + ", values: " + JSON.stringify(args));
+        cb.apply(this, args);
+        log(3, " > " + queue);
+        setTimeout(queue.execDeferred, 1, queue, ++i); // Now chain...
+      } catch (error) {
+        log(1, " > Received error while calling PQueue.add(..) callback, deferring to caught chain!");
+        setTimeout(queue.execCaught, 1, queue, error, 0);
       }
     }
-    
-    console.log(" > Invoking w/ " + args.length + " arg" + (args.length > 1 ? "s" : "")  + ", values: " + JSON.stringify(args));
-
-    cb.apply(this, args);
   }
+
+  try {
+    args.push(fcb);
+    log(2, " > Invoking deffered w/ " + args.length + " arg" + (args.length > 1 ? "s" : "")  + ", values: " + JSON.stringify(args));
+    fn.apply(this, args);
+  } catch (e) {
+    log(2, " > ERROR: " + e);
+    setTimeout(queue.execCaught, 1, queue, e, 0);
+  }
+}
+
+PQueue.prototype.execCaught = function(queue, e, i) {
+  if (i < 0) throw new Error("Invalid 'caught' reference index!");
+
+  if (i >= queue.caught.length) {
+    log(1, " > Completed all caught, now invoking finals!");
+    setTimeout(queue.execFinals, 1, queue, 0);
+    return;
+  }
+
+  log(3, " > ---------------------------------------------------------------");
+  log(2, " > Invoking caught #" + (i+1));
+
+  var cb = queue.caught[i].cb;
+  var args = [];
+    
+  try {
+    args.push(queue.stack);
+    args.push(e);
+    log(3, " > Invoking deffered w/ " + args.length + " arg" + (args.length > 1 ? "s" : "")  + ", values: " + JSON.stringify(args));
+    cb.apply(this, args);
+    setTimeout(queue.execCaught, 1, queue, e, ++i); // Now chain...
+  } catch (e) {
+    log(1, " > ERROR: " + e);
+  }
+}
   
-  console.log("Completed");
+PQueue.prototype.execFinals = function(queue, i) {
+  if (i < 0) throw new Error("Invalid 'finals' reference index!");
+
+  if (i >= queue.finals.length) {
+    log(1, " > Completed all finals, leaving!");
+    return;
+  }
+
+  log(3, " > ---------------------------------------------------------------");
+  log(2, " > Invoking finals #" + (i+1));
+
+  var cb = queue.finals[i].cb;
+  var args = [];
+    
+  try {
+    args.push(queue.stack);
+    log(3, " > Invoking deffered w/ " + args.length + " arg" + (args.length > 1 ? "s" : "")  + ", values: " + JSON.stringify(args));
+    cb.apply(this, args);
+    setTimeout(queue.execFinals, 1, queue, ++i); // Now chain...
+  } catch (e) {
+    log(2, " > ERROR: " + e);
+  }
 }
 
 PQueue.prototype.getStack = function() {
@@ -102,6 +215,7 @@ PQueue.prototype.getStack = function() {
 } 
 
 PQueue.prototype.ref = function(key) {
+  log(3, "Adding stack reference to key: '" + key + "', stack: " + this.stack);
   return new PStackRef(this.stack, key);
 }
 
@@ -144,12 +258,12 @@ PStack.prototype.getSize = function() {
 PStack.prototype.setValue = function(key, value) {
   this.size++;
   this.values[key] = value;
-  console.log(" > Setting stack key '" + key + "' as '" + value + "' (type " + (typeof value) + ")");
+  log(3, " > Setting stack key '" + key + "' as '" + value + "' (type " + (typeof value) + ")");
 }
 
 PStack.prototype.getValue = function(key) {
   var value = this.values[key];
-  console.log(" > Getting stack key '" + key + "' as '" + value + "' (type " + (typeof value) + ")");
+  log(3, " > Getting stack key '" + key + "' as '" + value + "' (type " + (typeof value) + ")");
   return value;
 }
 
@@ -179,11 +293,20 @@ function createQueue() {
  * @since  0.1.0
  */
 function version() {
-  return "PQueues v0.1.0";
+  return "0.1.0";
+}
+
+/**
+ * Boolean = true to log level 3, or false to turn off logging.
+ * Number  = 0=none, 1=info, 2=some, 3=full
+ */
+function debug(level) {
+  if (typeof level === 'boolean') logLevel = level ? 3 : 0;
+  if (typeof level === 'number') logLevel = level >= 0 ? level : 0;
 }
 
 function toString() {
-  var msg = "length: " + queues.length;
+  var msg = "[jeach-pqueues] version: " + this.version() + ", pqueues: " + queues.length;
   
   if (arguments.length > 0) {
     for (var i=0; i<queues.length; i++) {
@@ -193,6 +316,29 @@ function toString() {
   
   return msg;
 }
+
+function log() {
+  if (logLevel === 0) return;
+
+  var len = arguments.length;
+  var lev = 3;
+  var msg = null;
+
+  switch (len) {
+    case 1: 
+      lev = 3;
+      msg = arguments[0];
+      break;
+    case 2:
+      lev = arguments[0];
+      msg = arguments[1];
+      break;
+  }
+
+  if (logLevel > 0 && logLevel >= lev && msg) {
+    console.log("[" + new Date().toISOString() + "][" + lev + "]> " + msg);
+  }
+}
  
 
 /**
@@ -201,6 +347,7 @@ function toString() {
 module.exports = (function() {
   return {
     version: version,
+    debug: debug,
     createQueue: createQueue,
     toString: toString
   };
